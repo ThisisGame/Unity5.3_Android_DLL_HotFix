b0171d2b38e86a352bc2308dd2924154b4b4b732
captain <980097191@qq.com>
2017-02-27 20:30:08 +0800

    upload

---
+++ Assets/CopyAssembly.cs
@@ -0,0 +1,82 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using System.IO;
+
+public class CopyAssembly : MonoBehaviour
+{
+    [SerializeField]
+    Text mText;
+
+
+    private AndroidJavaClass _helper = null;
+
+    // Use this for initialization
+    void Start ()
+    {
+        mText.text = "1";
+
+        _helper = new AndroidJavaClass("com.cytx.tools.helper");
+        using (AndroidJavaClass jc = new AndroidJavaClass("com.unity3d.player.UnityPlayer"))
+        {
+            object jo = jc.GetStatic<AndroidJavaObject>("currentActivity");
+            _helper.CallStatic("init", jo);
+        }
+    }
+
+
+    void OnGUI()
+    {
+        if(GUI.Button(new Rect(100,100,100,100),"CopyAssembly"))
+        {
+            StartCoroutine(CopyAssemblyDLL());
+        }
+    }
+
+    IEnumerator CopyAssemblyDLL()
+    {
+        yield return 0;
+        WWW tmpWWW = new WWW("jar:file://" + Application.dataPath + "!/assets/Assembly-CSharp.dll");
+        yield return tmpWWW;
+
+        string datapath = Application.dataPath;
+        int start = datapath.IndexOf("com.");
+        int end = datapath.IndexOf("-");
+        string packagename = datapath.Substring(start, end - start);
+        string path = "/data/data/" + packagename + "/files/";
+        string dllpath = path + "Assembly-CSharp.dll";
+        if (File.Exists(dllpath))
+        {
+            Debug.Log("File Exists");
+        }
+        else
+        {
+            if(Directory.Exists(path)==false)
+            {
+                Directory.CreateDirectory(path);
+            }
+            using (FileStream fs = new FileStream(dllpath, FileMode.CreateNew))
+            {
+                fs.Write(tmpWWW.bytes,0,tmpWWW.bytes.Length);
+                fs.Flush();
+                fs.Close();
+            }
+        }
+
+        yield return new WaitForEndOfFrame();
+
+        restartApplication();
+    }
+
+    void restartApplication()
+    {
+        Debug.Log("restartApplication0");
+        _helper.CallStatic("restartApplication");
+        Debug.Log("restartApplication2");
+    }
+
+    // Update is called once per frame
+    void Update () {
+	
+	}
+}
---
+++ Assets/HotFix/Editor/BuildPostprocessor.cs
@@ -0,0 +1,47 @@
+﻿/** 
+ * 文件名:BuildPostprocessor.cs 
+ * Des:在导出Eclipse工程之后对替换mono.so
+ * Author:Captain 
+ * **/
+
+
+using UnityEngine;
+using UnityEditor;
+using UnityEditor.Callbacks;
+using System.IO;
+
+public class BuildPostprocessor
+{
+    [PostProcessBuildAttribute(1)]
+    public static void OnPostprocessBuild(BuildTarget target, string pathToBuiltProject)
+    {
+        if (target == BuildTarget.Android && (!pathToBuiltProject.EndsWith(".apk")))
+        {
+            Debug.Log("target: " + target.ToString());
+            Debug.Log("pathToBuiltProject: " + pathToBuiltProject);
+            Debug.Log("productName: " + PlayerSettings.productName);
+
+            Debug.Log("Current is : " + EditorUserBuildSettings.development.ToString());
+
+            //替换 libmono.so;  
+            if (EditorUserBuildSettings.development)
+            {
+                string armv7a_so_path = pathToBuiltProject + "/" + PlayerSettings.productName + "/" + "libs/armeabi-v7a/libmono.so";
+                File.Copy(Application.dataPath + "/HotFix/Editor/libs/development/armeabi-v7a/libmono.so", armv7a_so_path, true);
+
+                string x86_so_path = pathToBuiltProject + "/" + PlayerSettings.productName + "/" + "libs/x86/libmono.so";
+                File.Copy(Application.dataPath + "/HotFix/Editor/libs/development/x86/libmono.so", x86_so_path, true);
+            }
+            else
+            {
+                string armv7a_so_path = pathToBuiltProject + "/" + PlayerSettings.productName + "/" + "libs/armeabi-v7a/libmono.so";
+                File.Copy(Application.dataPath + "/HotFix/Editor/libs/release/armeabi-v7a/libmono.so", armv7a_so_path, true);
+
+                string x86_so_path = pathToBuiltProject + "/" + PlayerSettings.productName + "/" + "libs/x86/libmono.so";
+                File.Copy(Application.dataPath + "/HotFix/Editor/libs/release/x86/libmono.so", x86_so_path, true);
+            }
+
+            Debug.Log("HotFix libmono.so Success !!");
+        }
+    }
+}
